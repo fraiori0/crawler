Corrected and tested COM Jacobian function

crawler.py
@@ -4,6 +4,18 @@ import time
 import pybullet_data
 from math import *
 
+class Discretel_Low_Pass:
+    def __init__(self, dt, tc, K=1):
+        self.x = 0
+        self.dt = dt
+        self.tc = tc
+        self.K = K
+    def reset(self):
+        self.x = 0
+    def filter(self, signal):
+        self.x = (1-self.dt/self.tc)*self.x + self.K*self.dt/self.tc * signal
+        return self.x
+
 class Crawler:
 
     def __init__(self, spine_segments, urdf_path="/home/fra/Uni/Tesi/crawler", base_position=[0,0,0.2], base_orientation=[0,0,0,1]):
@@ -139,31 +151,30 @@ class Crawler:
     def get_joints_speeds_tuple(self):
         return list(zip(*(p.getJointStates(self.Id,list(range(0,self.num_joints))))))[1]
 
-    def prova_jacobian(self, link_index, base_eu_d):
-        #base_eu_d is a tuple containing the derivatives of the euler's angles describing the base orientation
-        self.set_links_state_array()
-        COM_vel = np.asarray(p.getLinkState(self.Id, link_index, computeLinkVelocity=1)[6])
-        joints_pos = self.get_joints_pos_tuple()
-        Jt, Jr = np.asarray(
-            p.calculateJacobian(self.Id,
-                link_index,
-                self.links_state_array[link_index]["loc_com_trn"],
-                joints_pos,
-                #[0.0]*(self.num_joints),
-                self.get_joints_speeds_tuple(),
-                [0.0]*(self.num_joints))
-            )
-        print("\nJt", Jt)
-        print("\nJr", Jr)
-        qdot = np.asarray(p.getBaseVelocity(self.Id)[1] + p.getBaseVelocity(self.Id)[0] + self.get_joints_speeds_tuple())
-        jacobian_vel= np.dot(Jt,qdot)
-        print("LINK %d" %link_index)
-        print("True COM speed: ",COM_vel,"\nComputed w/ Jacobian: ",jacobian_vel)
-        return
+    # def prova_jacobian(self, link_index, base_eu_d):
+    #     #base_eu_d is a tuple containing the derivatives of the euler's angles describing the base orientation
+    #     self.set_links_state_array()
+    #     COM_vel = np.asarray(p.getLinkState(self.Id, link_index, computeLinkVelocity=1)[6])
+    #     joints_pos = self.get_joints_pos_tuple()
+    #     Jt, Jr = np.asarray(
+    #         p.calculateJacobian(self.Id,
+    #             link_index,
+    #             self.links_state_array[link_index]["loc_com_trn"],
+    #             joints_pos,
+    #             #[0.0]*(self.num_joints),
+    #             self.get_joints_speeds_tuple(),
+    #             [0.0]*(self.num_joints))
+    #         )
+    #     print("\nJt", Jt)
+    #     print("\nJr", Jr)
+    #     qdot = np.asarray(p.getBaseVelocity(self.Id)[1] + p.getBaseVelocity(self.Id)[0] + self.get_joints_speeds_tuple())
+    #     jacobian_vel= np.dot(Jt,qdot)
+    #     print("LINK %d" %link_index)
+    #     print("True COM speed: ",COM_vel,"\nComputed w/ Jacobian: ",jacobian_vel)
+    #     return
     
     def get_base_Eulers(self):
-        eu = p.getEulerFromQuaternion(p.getBasePositionAndOrientation(self.Id)[1])
-        return  eu
+        return  p.getEulerFromQuaternion(p.getBasePositionAndOrientation(self.Id)[1])
 
     def COM_trn_jacobian(self):
         #Jacobian of the COM is computed as the weighted mean (with respect to masses) of the Jacobian of the links
@@ -171,9 +182,9 @@ class Crawler:
             # plus null terms associated to the base angular speed and the joints speed terms (thus 3+self.num_joints)
             # NOTE: angular speed values are null just because the COM of the girdle is at the origin of the link frame
         self.set_links_state_array()
-        Jbase_t = np.asarray([[1,0,0]+[0]*(3+self.num_joints),
-                            [0,1,0]+[0]*(3+self.num_joints),
-                            [0,0,1]+[0]*(3+self.num_joints) ])
+        Jbase_t = np.asarray([  [0.0]*3 + [1.0,0.0,0.0] + [0.0]*(self.num_joints),
+                                [0.0]*3 + [0.0,1.0,0.0] + [0.0]*(self.num_joints),
+                                [0.0]*3 + [0.0,0.0,1.0] + [0.0]*(self.num_joints) ])
         JM_t = Jbase_t*(p.getDynamicsInfo(self.Id,-1)[0])
         joints_pos = self.get_joints_pos_tuple()
         for i in range(0,self.num_joints):
@@ -182,7 +193,7 @@ class Crawler:
                     i,
                     self.links_state_array[i]["loc_com_trn"],
                     joints_pos,
-                    [0.0]*(self.num_joints),
+                    self.get_joints_speeds_tuple(),
                     [0.0]*(self.num_joints))
                 )[0]
             JM_t += Ji_t * (p.getDynamicsInfo(self.Id,i)[0])

sim_crawler.py
@@ -4,39 +4,22 @@ import matplotlib.pyplot as plt
 import matplotlib
 import time
 import pybullet_data
-import crawler
+import crawler as crw
 from math import *
 
 matplotlib.use('TkAgg')
-
-class Discretel_Low_Pass:
-    def __init__(self, dt, tc, K=1):
-        self.x = 0
-        self.dt = dt
-        self.tc = tc
-        self.K = K
-    def reset(self):
-        self.x = 0
-    def filter(self, signal):
-        self.x = (1-self.dt/self.tc)*self.x + self.K*self.dt/self.tc * signal
-        return self.x
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.81)
 planeID = p.loadURDF("plane100.urdf", [0,0,0])
-model = crawler.Crawler(spine_segments=8,base_position=[0,0,1])
-#for i in range (500):
-#  p.stepSimulation()
-#  p.applyExternalForce(crawlerID, -1, [np.random.random_sample(), 3*np.random.random_sample(), np.random.random_sample()], [0, 0, 0], flags=p.WORLD_FRAME)
-#  time.sleep(1./240.)
+model = crw.Crawler(spine_segments=8,base_position=[0,0,1])
 #time-step used for simulation and total time passed variable
 dt = 1./240.
 t = 0.0
 ###
 model.turn_off_crawler()
-print("COM Jacobian:\n",model.COM_trn_jacobian,"\n\n")
-low_pass = Discretel_Low_Pass(dt,tc=0.5,K=1)
+low_pass = crw.Discretel_Low_Pass(dt,tc=0.5,K=1)
 eu0 = np.array((0,0,0))
 eu1 = np.array((0,0,0))
 eud_listx = list()
@@ -48,33 +31,28 @@ for i in range (100):
     #     np.random.random_sample()],
     #     [0, 0, 0], flags=p.LINK_FRAME)
     COM_prev = model.COM_position_world()
-    eu0 = eu1
+    #eu0 = eu1
     ###
     p.stepSimulation()
     ### 
     COM_curr = model.COM_position_world()
     p.addUserDebugLine(COM_prev.tolist(), COM_curr.tolist(), lineColorRGB=[sin(4*pi*t),sin(4*pi*(t+0.33)),sin(4*pi*(t+0.67))],lineWidth=3, lifeTime=2)
-    eu1 = np.asarray(model.get_base_Eulers())
-    eud = (eu0-eu1)/dt
-    eud_f = np.asarray(
-        (low_pass.filter((eu0[0]-eu1[0])/dt),
-        low_pass.filter((eu0[1]-eu1[1])/dt),
-        low_pass.filter((eu0[2]-eu1[2])/dt)
-        )
-    )
-    eud_listx.append(eud[1])
-    eud_listx_filtered.append(eud_f[1])
+    # eu1 = np.asarray(model.get_base_Eulers())
+    # eud = (eu0-eu1)/dt
+    # eud_f = np.asarray(
+    #     (low_pass.filter((eu0[0]-eu1[0])/dt),
+    #     low_pass.filter((eu0[1]-eu1[1])/dt),
+    #     low_pass.filter((eu0[2]-eu1[2])/dt)
+    #     )
+    # )
+    # eud_listx.append(eud[1])
+    # eud_listx_filtered.append(eud_f[1])
     ###
     time.sleep(dt)
     t+=dt
-# qdot = np.asarray(p.getBaseVelocity(model.Id)[0] + p.getBaseVelocity(model.Id)[1] + model.get_joints_speeds_tuple())
-# j_COM_speed = np.dot(model.COM_trn_jacobian(),qdot)
-# print("True COM linear speed:  ", model.COM_velocity_world(),"\n","Linear speed computed w/ Jacobian:  ",j_COM_speed)
-# print(model.COM_trn_jacobian())
-fig, ax = plt.subplots()
-plt.plot(eud_listx,"r")
-plt.plot(eud_listx_filtered,"b")
-plt.show()
-model.prova_jacobian(7,base_eu_d=tuple(eud_f))
+qdot = np.asarray(p.getBaseVelocity(model.Id)[1] + p.getBaseVelocity(model.Id)[0] + model.get_joints_speeds_tuple())
+j_COM_speed = np.dot(model.COM_trn_jacobian(),qdot)
+print("True COM linear speed:  ", model.COM_velocity_world(),"\n","Linear speed computed w/ Jacobian:  ",j_COM_speed)
+print(model.COM_trn_jacobian())
 
 p.disconnect()