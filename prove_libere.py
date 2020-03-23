6cdcad2687ddd65dec5fd9e310c1712814fb445d
Francesco Iori <fraiori@hotmail.it>
2020-03-23 10:51:31 +0100

    Torque control and more

--- prove_libere.py
+++ prove_libere.py
@@ -1,14 +1,157 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from matplotlib.pyplot import plot
 import pybullet as p
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+import time
+import pybullet_data
+import crawler as crw
 from math import *
-import crawler
+import pinocchio as pin
+import pyparsing
 
+
+matplotlib.use('TkAgg')
+
+urdf_path="/home/fra/Uni/Tesi/crawler"
+urdf_filename=urdf_path+"/crawler.urdf"
+
+#########################################
+####### SIMULATION SET-UP ###############
+#########################################
+### time-step used for simulation and total time passed variable
+dt = 1./240.
+#
 physicsClient = p.connect(p.GUI)
-model = crawler.Crawler(dt_simulation=1/240)
-print(model.control_indices)
-print(p.getLinkState(model.Id,model.control_indices[2])[0])
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0,0,-9.81)
+#p.setGravity(0.0,0.0,0.0)
+#planeID = p.loadURDF("plane100.urdf", [0,0,0])
+model = crw.Crawler(urdf_path=urdf_path, dt_simulation=dt, base_position=[0,0,0.5], base_orientation=[0,0,0,1])
+np.random.seed()
+
+model.set_low_pass_qd(fc=7000)
+for index in range(-1,model.num_joints):
+    p.changeDynamics(
+        model.Id,
+        index,
+        linearDamping=0.0,
+        angularDamping=0.0
+    )
+# qd_prev = model.get_joints_speeds_tuple()
+# qdd_curr = model.get_joints_acc_nparr(qd_prev=qd_prev)
+#print(model.solve_torques_lateral(np.array(list(model.control_indices[0])),qdd_curr))
+model.turn_off_crawler()
+# for i in range (240):
+#     p.stepSimulation()
+#     time.sleep(dt)
+#model.turn_off_crawler()
+###
+M = p.calculateMassMatrix(model.Id, model.get_joints_pos_tuple())
+M = np.array(M)
+Ma = M[:,model.mask_act][model.mask_act,:]
+print(np.round(M,3))
+print("DIM M = ", M.shape)
+print(np.round(Ma,3))
+print("DIM Ma = ", Ma.shape)
+###
+q_list = list(model.get_joints_pos_tuple()) + ([0]*6)
+qd_list = list(model.get_joints_speeds_tuple())
+qd_list = [1]*(model.num_joints+6)
+qdd_des_list= [1]*(model.num_joints+6)
+tau_exact = np.array(
+    p.calculateInverseDynamics(
+        model.Id,
+        q_list,
+        qd_list,
+        qdd_des_list,
+        flags=1
+        )
+    )
+print("TAU EXACT\n", tau_exact)
+print("Q = ", model.get_q())
+model.set_velocities()
+p.stepSimulation()
+time.sleep(dt)
+model.set_velocities()
+print("qd_prev", model.qd_prev)
+print("qd_curr", model.qd_curr)
+qdd_history = np.array(([0]*(model.num_joints+6)))
+qdd_f_history = np.array(([0]*(model.num_joints+6)))
+model.turn_off_crawler()
+for i in range(20):
+    p.stepSimulation()
+    model.set_velocities()
+    q = model.get_q()
+    qd = model.qd_curr
+    acc = model.get_qdd_nparr()
+    qdd = acc[0]
+    qdd_f = acc[1]
+    qdd_history = np.vstack((qdd_history,qdd))
+    qdd_f_history = np.vstack((qdd_f_history,qdd_f))
+    time.sleep(dt)
+print("\nEEEEEEEEEEEEEEEEEEEEEEEEEEEEE",qdd,"\n")
+
+# fig, ax = plt.subplots()
+# plt.plot(qdd_history[:,6],"b")
+# plt.plot(qdd_f_history[:,6],"r")
+# #plt.plot(qdaf3,"r")
+# plt.show()
+# print(qdd)
+###
+
+#########################################
+####### PINOCCHIO TEST ##################
+#########################################
+pinmodel = pin.buildModelFromUrdf(urdf_filename,pin.JointModelFreeFlyer())
+print('model name: ' + pinmodel.name)
+# Create data required by the algorithms
+data = pinmodel.createData()
+###
+#qdd = np.array(([0]*(model.num_joints+6)))
+###
+print("\n\n\nYOHHHHHHH")
+q_pin = q[model.mask_q_pyb_to_pin]
+qd_pin = qd[model.mask_qd_pyb_to_pin]
+qdd_pin = qdd[model.mask_qd_pyb_to_pin]
+# q_pin[2]=1
+# qd_pin[2]=1
+# qdd_pin[2]=-9.81
+print("Q_PIN\n",np.round(q_pin,5))
+print("QD_PIN\n",np.round(qd_pin,5))
+print("QDD_PIN\n",np.round(qdd_pin,5))
+pin.crba(pinmodel,data,q_pin)
+pin.computeCoriolisMatrix(pinmodel,data,q_pin,qd_pin)
+datag = pin.computeGeneralizedGravity(pinmodel,data,q_pin)
+pin.rnea(pinmodel,data,q_pin,qd_pin,qdd_pin)
+print("M\n",np.round(data.M,5))
+print("C\n",np.round(data.C,5))
+print("g\n",np.round(datag,5))
+print("TAU_PIN\n",np.round(data.tau,4))
+print("\n")
+indices = list(range(model.num_joints))
+joint_states = p.getJointStates(model.Id,indices)
+joint_states_np = np.array(joint_states)
+tau_real_np = joint_states_np[:,3]
+print("TAU REAL: ",tau_real_np)
+print("\n\n\n")
+
+###
+# Sample a random configuration
+# print("Q RANDOM")
+# q = pin.randomConfiguration(pinmodel)
+# print(q)
+# print(q.shape)
+#print('q: %s' % q.T)
+# Perform the forward kinematics over the kinematic tree
+
+# print(pin.rnea(pinmodel,data,np.array(([0]*(pinmodel.nq+6))),np.array(([0]*(pinmodel.nq+6))),np.array(([0]*(pinmodel.nq+6)))))
+# Print out the placement of each joint of the kinematic tree
+# pin.forwardKinematics(pinmodel,data,q)
+# for name, oMi, tau in zip(pinmodel.names, data.oMi, data.tau):
+#     print(("{:<24} : {: .2f} {: .2f} {: .2f} : {: .2f}"
+#         .format( name, *oMi.translation.T.flat, tau )))
+# print(model.mask_q_py_to_pin)
+# print(model.mask_qd_py_to_pin)
 p.disconnect()
 
 
